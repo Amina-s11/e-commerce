c8bbe9aecf7a9c389ee2f6a370a9f041e4d4473d
Amina-s11 <amina291124@gmail.com>
2026-04-15 21:58:23 +0530

    backend update on 15th April

---
+++ admin/src/assets/data.js
@@ -0,0 +1,34 @@
+import p1_img from './product_1.png'
+import p2_img from './product_2.png'
+import p3_img from './product_3.png'
+import p4_img from './product_4.png'
+
+let data_product = [
+  {
+    id:1,
+    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image:p1_img,
+    new_price:54.00,
+    old_price:72.50,
+  },
+  {id:2,
+    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image:p2_img,
+    new_price:41.00,
+    old_price:60.50,
+  },
+  {id:3,
+    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image:p3_img,
+    new_price:37.00,
+    old_price:52.50,
+  },
+  {id:4,
+    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image:p4_img,
+    new_price:49.00,
+    old_price:63.00,
+  },
+];
+
+export default data_product;
