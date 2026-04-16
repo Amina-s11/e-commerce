b09cb8d9dbff2c9a426ff17d7b273008a22fe251
Amina-s11 <amina291124@gmail.com>
2026-04-16 20:44:20 +0530

    Admin

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import './Navbar.css'
-import navlogo from '../../assets/nav-logo.svg'
+import logo from '../../assets/logo.png'
 import navProfile from '../../assets/nav-profile.svg'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-            <img src={navlogo} alt="" className="nav-logo" />
+      <div className="navlogo">
+        <img src={logo} alt="" className="navlogo" />
+            <div className="text">
+              <h1>FASHIONEXT</h1>
+              <p>Admin Panel</p>
+            </div>
+      </div>
             <img src={navProfile} className='nav-profile' alt="" />
     </div>
   )
